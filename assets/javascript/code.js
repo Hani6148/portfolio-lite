330925200c024dffe798266bd0a409a7259c3f68
hani6148 <hanikhellafi@gmail.com>
2021-06-17 03:19:09 -0500

    changes

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -4,9 +4,11 @@ vh = window.innerHeight
 
 
 $(document).on("scroll", (function () {
-   //calculating scroll position on scroll event 
-  //making style changes to the link corresponding to the page section according to the scroll position 
+    //calculating scroll position on scroll event 
+    //making style changes to the link corresponding to the page section according to the scroll position 
     pos = document.documentElement.scrollTop
+    console.log("pos", pos)
+    console.log("vh", vh)
     if (pos === 0) {
         $(".menu-link").css("writing-mode", "vertical-rl");
         $(".menu-link").css("font-size", "1rem");
@@ -16,7 +18,7 @@ $(document).on("scroll", (function () {
         $("#link1").css("background", "linear-gradient(0deg,rgb(30 164 194 / 72%) 50%, transparent 50%)");
     }
 
-    else if (pos >= vh && pos <= vh + 2) {
+    else if (pos >= vh - 5 && pos <= vh + 5) {
         $(".menu-link").css("writing-mode", "vertical-rl");
         $(".menu-link").css("font-size", "1rem");
         $(".menu-link").css("background", "white");
@@ -25,7 +27,7 @@ $(document).on("scroll", (function () {
         $("#link2").css("background", "linear-gradient(0deg,rgb(5 24 50 / 33%) 50%, transparent 50%)");
     }
 
-    else if (pos >= vh * 2 && pos <= vh * 2 + 2) {
+    else if (pos >= vh * 2 - 5 && pos <= vh * 2 + 5) {
         $(".menu-link").css("writing-mode", "vertical-rl");
         $(".menu-link").css("font-size", "1rem");
         $(".menu-link").css("background", "white");
@@ -33,7 +35,7 @@ $(document).on("scroll", (function () {
         $("#link3").css("font-size", "1.5rem");
         $("#link3").css("background", "linear-gradient(0deg,#b0bdcb 50%, transparent 50%)");
     }
-    else if (pos >= vh * 3 && pos <= vh * 3 + 2) {
+    else if (pos >= vh * 3 - 5 && pos <= vh * 3 + 5) {
         $(".menu-link").css("writing-mode", "vertical-rl");
         $(".menu-link").css("font-size", "1rem");
         $(".menu-link").css("background", "white");
@@ -43,17 +45,17 @@ $(document).on("scroll", (function () {
     }
 
     //bring the .back section forward if the scroll position is bigger than the height of the viewport + 1 
-    if ((pos > vh*2+1)) {
+    if ((pos > vh * 2 - 5)) {
         $(".back").css("z-index", "5");
-       
+
     }
     else {
-         $(".back").css("z-index", "1");
-         
-            }
+        $(".back").css("z-index", "1");
+
+    }
 
 }))
- 
+
 
 
 
